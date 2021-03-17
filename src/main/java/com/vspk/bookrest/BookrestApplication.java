fdaeefa7941350e5a9b6465ef2b892aca50f9e80
Vovaspk <carlostevez228@gmail.com>
2021-03-17 19:23:40 +0200

    Initial project template created

---
+++ src/main/java/com/vspk/bookrest/BookrestApplication.java
@@ -0,0 +1,13 @@
+package com.vspk.bookrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookrestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookrestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vspk/bookrest/BookrestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vspk.bookrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookrestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
