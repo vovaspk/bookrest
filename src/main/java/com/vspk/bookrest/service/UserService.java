ff6fc0acbdcf808cd18cb574f551865e0bf8a96e
Vovaspk <carlostevez228@gmail.com>
2021-04-02 23:20:30 +0300

    add user service

---
+++ src/main/java/com/vspk/bookrest/service/UserService.java
@@ -0,0 +1,17 @@
+package com.vspk.bookrest.service;
+
+import com.vspk.bookrest.domain.User;
+
+import java.util.List;
+
+public interface UserService {
+    User register(User user);
+
+    List<User> getAll();
+
+    User findByUsername(String username);
+
+    User findById(Long id);
+
+    void delete(Long id);
+}
---
+++ src/main/java/com/vspk/bookrest/service/impl/UserServiceImpl.java
@@ -0,0 +1,73 @@
+package com.vspk.bookrest.service.impl;
+
+import com.vspk.bookrest.domain.Role;
+import com.vspk.bookrest.domain.Status;
+import com.vspk.bookrest.domain.User;
+import com.vspk.bookrest.repository.RoleRepository;
+import com.vspk.bookrest.repository.UserRepository;
+import com.vspk.bookrest.service.UserService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Slf4j
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
+    private final BCryptPasswordEncoder passwordEncoder;
+
+    @Override
+    public User register(User user) {
+        Role roleUser = roleRepository.findByName("ROLE_USER");
+        List<Role> userRoles = new ArrayList<>();
+        userRoles.add(roleUser);
+
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        user.setRoles(userRoles);
+        user.setStatus(Status.ACTIVE);
+
+        User registeredUser = userRepository.save(user);
+
+        log.info("IN register - user: {} successfully registered", registeredUser);
+
+        return registeredUser;
+    }
+
+    @Override
+    public List<User> getAll() {
+        List<User> result = userRepository.findAll();
+        log.info("IN getAll - {} users found", result.size());
+        return result;
+    }
+
+    @Override
+    public User findByUsername(String username) {
+        User result = userRepository.findByUsername(username);
+        log.info("IN findByUsername - user: {} found by username: {}", result, username);
+        return result;
+    }
+
+    @Override
+    public User findById(Long id) {
+        User result = userRepository.findById(id).orElse(null);
+
+        if (result == null) {
+            log.warn("IN findById - no user found by id: {}", id);
+            return null;
+        }
+
+        log.info("IN findById - user: {} found by id: {}", result, id);
+        return result;
+    }
+
+    @Override
+    public void delete(Long id) {
+        userRepository.deleteById(id);
+        log.info("IN delete - user with id: {} successfully deleted", id);
+    }
+}
