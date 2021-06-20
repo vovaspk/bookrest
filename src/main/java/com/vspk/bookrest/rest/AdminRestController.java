1022ceaa66c7393aab16ba7203b5ffb9d27c434b
Vovaspk <carlostevez228@gmail.com>
2021-06-20 17:57:27 +0300

    add admin endpoint, enable cors, update log4j

---
+++ src/main/java/com/vspk/bookrest/rest/AdminRestController.java
@@ -0,0 +1,35 @@
+package com.vspk.bookrest.rest;
+
+import com.vspk.bookrest.domain.User;
+import com.vspk.bookrest.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.security.RolesAllowed;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/api/v1/admin/")
+@RolesAllowed("ADMIN")
+@RequiredArgsConstructor
+@CrossOrigin(origins = "*")
+public class AdminRestController {
+
+    private final UserService userService;
+
+
+    @GetMapping(value = "users")
+    public List<User> getUsers(){
+        return userService.getAll();
+    }
+
+    @DeleteMapping(value = "users/{id}")
+    public void getUsers(@PathVariable Long id) {
+        userService.delete(id);
+    }
+}
